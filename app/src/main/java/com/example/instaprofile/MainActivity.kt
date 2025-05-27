69db453861d681056132f18afc4a0ccfb714ec74
Алексей <mr-kaifat@yandex.ru>
2025-05-27 14:50:09 +0300

    обновлено

--- app/src/main/java/com/example/instaprofile/MainActivity.kt
+++ app/src/main/java/com/example/instaprofile/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
 import com.example.instaprofile.ui.theme.InstaProfileTheme
 import com.example.instaprofile.ui.theme.InstagramProfileCard
 
@@ -13,9 +18,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             InstaProfileTheme {
+                Box (modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+                ) {
                     InstagramProfileCard()
+                }
 
             }
         }
     }
 }
+
--- app/src/main/java/com/example/instaprofile/ui/theme/InstgramProfileCard.kt
+++ app/src/main/java/com/example/instaprofile/ui/theme/InstgramProfileCard.kt
@@ -1,5 +1,6 @@
 package com.example.instaprofile.ui.theme
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -8,53 +9,82 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-@Preview(showBackground = true)
 @Composable
 fun InstagramProfileCard(){
-    Row(modifier = Modifier
-        .fillMaxWidth()
-        .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
-    ){
-        Box(modifier = Modifier
-            .size(50.dp)
-            .background(color = Color.Green)) {
+    Card(modifier = Modifier
+        .padding(8.dp)
+        .background(color = MaterialTheme.colorScheme.background),
+        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground)
+        ) {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Box(modifier = Modifier
+                .size(50.dp)
+                .background(color = Color.Green)) {
 
+            }
+            UserStatistics(title = "Posts", value = "6,950")
+
+            UserStatistics(value = "436M", title = "Followers")
+
+            UserStatistics(value = "76", title = "Following")
         }
-        TwoBoxes(
-            //Text(text = "6,950"),
-            //Text(text = "6,950")
-        )
-        TwoBoxes(
-            //Text(text = "436M"),
-            //Text(text = "Followers")
-        )
-        TwoBoxes(
-            //Text(text = "76"),
-            //Text(text = "Following")
-        )
     }
+
 }
 
 @Composable
-private fun TwoBoxes(){
+private fun UserStatistics(
+    title: String,
+    value: String
+){
     Column(modifier = Modifier ,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
-        Box(modifier = Modifier
-            .size(50.dp)
-            .background(color = Color.Yellow))
-        Box(modifier = Modifier
-            .size(50.dp)
-            .background(color = Color.Gray))
+        Text(text = value, fontSize = 32.sp , fontFamily = FontFamily.Cursive )
+        Text(text = title, fontWeight = Bold )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardLight(){
+    InstaProfileTheme(
+        darkTheme = false
+    ) {
+
+        InstagramProfileCard()
+
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardDark(){
+    InstaProfileTheme(
+        darkTheme = true
+    ) {
+        InstagramProfileCard()
+
     }
 }
